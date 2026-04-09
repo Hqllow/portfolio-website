7f7b862cc0a615895bb1d78f8aece4df528d12e8
Bradley Cederholm <106129785+Hqllow@users.noreply.github.com>
2026-04-09 19:40:12 -0400

    Add additional projects

--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,8 @@ import { LuGraduationCap } from "react-icons/lu";
 import simImg from "@/public/flaresim.png";
 import telemImg from "@/public/telem-app.png";
 import nasaImg from "@/public/adc-moon.png";
+import obamaImg from "@/public/obama.png";
+import wikiImg from "@/public/wikirace.png";
 
 export const links = [
   {
@@ -61,14 +63,14 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "Flare Simulation",
+    title: "FlareSim 2",
     description:
       "Physics-based, iterative simulation to predict lap speeds, acceleration, and distance, to optimize solar car performance at FSGP. Features OpenMeteo weather forecasting to predict energy changes.",
     tags: ["React", "JavaScript", "Golang"],
     imageUrl: simImg,
   },
   {
-    title: "Telemetry Web App",
+    title: "Flare Telemetry Dash",
     description:
       "Real-time dashboard featuring streamed data from the Solar Gators car, Flare. Live GPS monitoring and historic data visualization pages, with full OAuth integration.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Neon", "OAuth"],
@@ -80,6 +82,20 @@ export const projectsData = [
       "A real-world, first-person perspective simulation of the moon's surface with simulated communication and real-time azimuth calculations.",
     tags: ["Unity", "C#", "Blender"],
     imageUrl: nasaImg,
+  },
+  {
+    title: "ObamaBot V2",
+    description:
+      "Feature-rich JDA-based Discord bot built with Maven, featuring modular slash commands, Mojang account validation, and scalable utility automation for responsive server management.",
+    tags: ["Java", "JDA", "Maven", "REST APIs", "Discord", "JSON"],
+    imageUrl: obamaImg,
+  },
+  {
+    title: "WikiRace",
+    description:
+      "High-performance SFML and C++ pathfinding tool that solves Wikipedia races across a graph of 7 million nodes using tree-based search, optimized traversal, and interactive visualization.",
+    tags: ["C++", "SFML", "Pathfinding", "Graphs", "Algorithms"],
+    imageUrl: wikiImg,
   },
 ] as const;
 
